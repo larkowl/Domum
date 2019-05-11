1c3179118b245f917a1693969ea2e0261ead0a58
Эмилия Кибенко <itsmevanster@gmail.com>
2019-05-11 21:01:55 +0300

    Yevtushenko: finctionality for viewing ads

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 
 
 class AnnouncementForm(forms.ModelForm):
-
     class Meta:
         model = Announcement
         fields = ('price', 'district', 'distance_to_metro', 'area', 'repairs', 'comment')
@@ -14,9 +13,13 @@ class AnnouncementForm(forms.ModelForm):
 
 
 class SignUpForm(UserCreationForm):
-    phone = forms.CharField(max_length=20)
-
+    phone = forms.CharField(max_length=20, required=False)
     class Meta:
         model = User
-        fields = ('username', 'phone', 'email', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name', 'phone', 'email', 'password1', 'password2')
+'''
+    def clean_due_back(self):
+        data = self.cleaned_data['due_back']
+        if 
+'''
 
